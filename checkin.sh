34fd6c9c1a18700eb92d8f3568f9f0a4b794d47f
lenovo <984187775@qq.com>
2023-07-02 21:53:54 +0800

    Initial commit

--- checkin.sh
+++ checkin.sh
@@ -3,7 +3,15 @@ curl -H "cookie:${COOKIE}" -H 'content-type:application/json;charset=UTF-8' -d '
 echo '-----------------status-----------------'
 curl -H "cookie:${COOKIE}" -X GET 'https://glados.rocks/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
 
+echo '------------------sign------------------'
+curl -H "cookie:${VAR1}" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.network"}' -X POST 'https://glados.rocks/api/user/checkin' | grep -Eo '"message":"[^"]*"'
+echo '-----------------status-----------------'
+curl -H "cookie:${VAR1}" -X GET 'https://glados.rocks/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
 
+echo '------------------sign------------------'
+curl -H "cookie:${VAR2}" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.network"}' -X POST 'https://glados.rocks/api/user/checkin' | grep -Eo '"message":"[^"]*"'
+echo '-----------------status-----------------'
+curl -H "cookie:${VAR2}" -X GET 'https://glados.rocks/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
 
 
 
